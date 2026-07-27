4d4684cf909d9c90e315cb7fc2e9f9638e771450
Kay Tanaka <kga6829@student.pps.net>
2026-07-27 13:26:39 -0700

    done

--- Sort_by_content.py
+++ Sort_by_content.py
@@ -3,12 +3,11 @@ import re
 from pathlib import Path
 
 folder_path = input("Provide path to folder with article .txt files: ")
-directory = Path(folder_path)
 
 #creates a list of the paths to all the txt files
 texts = []
 for i in range(30):
-    texts.append(directory / f"Paper{i+1}.txt")
+    texts.append(folder_path + f"/Paper{i+1}.txt")
 
 #creates a dictionary to houes the counts for each article/txt file
 all = {}
